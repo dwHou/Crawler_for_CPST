de2ffa4c667d77087af26353c49b0087e079646b
Kornberg <kornbergfresnel@outlook.com>
2016-10-21 11:27:51 +0800

    add some error process method
    1）使用GB2312解码过程遇到的特殊字符问题，添加ignore字段解决
    2）对于多次跳转的网页，采用js爬取
    3）对于需要内网访问的网页，在文件内部嵌入访问链接，需要联入内网

--- htmlcrawler.py
+++ htmlcrawler.py
@@ -1,48 +1,64 @@
 import httplib2
 import re
 from bs4 import BeautifulSoup
+import os
+import base64
 
 
 ENCODING = 'GB2312'
+DIR_BASE = 'news/'
 
 
 class HtmlCrawler(object):
-    def __init__(self, url_base, encoding):
-        """
+    def __init__(self, url_base, encoding, http, dir_base):
+        """Initialize some parameters
 
         :param url_base: the base url of a website, which will be used to get some resource
         :param encoding: the encoding used to decode html file
+        :param http: a http object used to send request
+        :param dir_base: if the news block is 'employment', then dir_base equals 'employment
         """
         self.url_base = url_base
         self.encoding = encoding
+        self.http = http
+        self.dir_base = dir_base + '/'
 
-    def handle_content(self, http, url, url_type='str'):
+    def _to_base64_(self, filename):
+        """Change filename to base64
+
+        :type filename: str
+        :param filename: file name
+        :return: str
         """
+        return base64.b64decode(filename)
 
-        :type http: httplib2.Http()
-        :param http: a http object
+    def _handle_save_(self, dir_name, content, files_path, news_title):
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name, mode=0o777)
+
+        filename = self._to_base64_(news_title)
+        file_path = dir_name + filename + '.html'
+        files_path.append(file_path)
+
+        with open(file_path, 'wb') as f:
+            f.write(content)
+
+    def handle_content(self, url):
+        """Get content by url
 
         :type url: str or list
         :param url: the url of a web-page
-
-        :type url_type: str
-        :param url_type: point that the type of url: str or list
         :return homepage
         """
 
-        if url_type == 'list':
-            homepage = []
-            for ele in url:
-                response, content = http.request(ele)
-                homepage.append(content.decode(self.encoding))
-        else:
-            response, homepage = http.request(url)
-            homepage = homepage.decode(self.encoding)
-
-        return homepage
+        response, content = self.http.request(url)
+        content = content.decode(self.encoding, 'ignore')
+        return content
 
     def handle_news_list(self, html, pattern):
-        """
+        """Get news list from a html-page, and split news into two parts:
+        news link -- used for get the content of a certain news
+        news title and stamp -- used fro get the title and stamp
 
         :type html: str
         :param html: a homepage which has a news list
@@ -54,57 +70,66 @@ class HtmlCrawler(object):
         """
         news_list = re.findall(string=html, pattern=pattern)  # get news list
         links = []  # store news' link
-        news_title_spam = []  # store news' title and its spam
+        news_spam = []  # store news' title and its spam
+        spam_pattern = re.compile(pattern='[0-9]{4}/[0-9]{1,2}/[0-9]{1,2} [0-9]{1,2}:[0-9]{2}:[0-9]{2}')
 
-        # for each news in news_list, we need get some resource of this news
-        # such as images related to this news, and the article
-        # the structure of news: news[0] -- link to article, news[1] -- title and spam
         for news in news_list:
             links.append(self.url_base + news[0])  # get the link to news's page
-            news_title_spam.append(news[1])
-        return links, news_title_spam
+            news_spam.append(spam_pattern.search(news[1]).group(0))
+        return links, news_spam
 
-    def handle_news_files(self, news_links, dir):
-        """
+    def handle_news_files(self, news_links, dirs_pa):
+        """Get fixed news files and store them in FTP
 
         :type news_links: a str list
         :param news_links: url oriented to news' page
-        :type dir: str
-        :param dir: the dir used to store news file
+        :type dirs_pa: str
+        :param dirs_pa: the dir parameters used to store news file, but dirs_pa is time spam
         :return: list, file paths
         :return: images_list: a list whose element is image
         """
-        x_content = self.handle_content(http=httplib2.Http(), url=news_links, url_type='list')  # get content of news' page
-        idx = 0
         files_path = []
-        image_links = []
+        title_and_img = []
 
-        for content in x_content:
+        for idx in range(len(news_links)):
+            content = self.handle_content(url=news_links[idx])
+            # get date, and combine DIR_BASE to get dir path
+            dir_name = DIR_BASE + self.dir_base + dirs_pa[idx].split()[0] + '/'
             soup = BeautifulSoup(content, 'html.parser')  # use Beautiful to parse the original page
-            # news_title = soup.title.string
+            if soup.title:
+                news_title = soup.title.string
 
-            article = soup.find('table', class_='right_news_lb')  # get the real article part
-            article_fix = article.contents[1]  # fix the real article part
-            images = article_fix.find_all('img')  # find all img tags from fixed article
+                article = soup.find('table', class_='right_news_lb')  # get the real article part
+                article_fix = article.contents[1]  # fix the real article part
+                images = article_fix.find_all('img')  # find all img tags from fixed article
 
-            # get image resource
-            if len(images) > 0:
-                # fix the original link, get the real url to image resource
-                temp = []
-                for img in images:
-                    img_link = self.url_base + img.attrs['src']
-                    img.attrs['src'] = img_link
-                    temp.append(img_link)
+                # get image resource, fix the original link, get the real url to image resource
+                if len(images) > 0:
+                    temp = []
+                    for img in images:
+                        img_link = self.url_base + img.attrs['src']
+                        img.attrs['src'] = img_link
+                        temp.append(img_link)
 
-                image_links.append(temp[0])
+                    title_and_img.append((news_title, temp[0]))
+                else:
+                    title_and_img.append((news_title, ''))
+
+                self._handle_save_(dir_name, article_fix.prettify('utf-8'), files_path, news_title)
             else:
-                image_links.append('')
+                # print('-->No String!: {0}, will solve..'.format(news_links[idx]))
+                redirect_pattern = re.compile('href=\'(.*?)\'')
+                redirect_url = re.findall(string=content, pattern=redirect_pattern)[0]
+                try:
+                    r, c = self.http.request(redirect_url)
+                    c = c.decode('gb2312', 'ignore')
+                    soup2 = BeautifulSoup(c, 'html.parser')
+                    news_title = soup2.title.string
+                    self._handle_save_(dir_name, c, files_path, news_title)
+                except:
+                    # print('-->Read time out at: {0}'.format(redirect_url))
+                    c1 = '<h1 style=\"text-align: center\">对不起！您访问的链接已损坏，或者需要校园网访问！</h1>'
+                    c2 = '<a style=\"text-align: center; display: block\" href=\"' + redirect_url + '\">确定接入校园网后，点击此处访问</a>'
+                    self._handle_save_(dir_name, (c1 + c2).encode(), files_path, 'Inner News')
 
-            file_path = dir + 'article_x' + str(idx) + '.html'
-            files_path.append(file_path)
-
-            with open(file_path, 'wb') as f:
-                f.write(article_fix.prettify('utf-8'))
-            idx += 1
-
-        return files_path, image_links
+        return files_path, title_and_img
