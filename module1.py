5933cdf5fd9890567be118bc36c2ae25be6b470e
Kornberg <kornbergfresnel@outlook.com>
2016-10-21 11:29:24 +0800

    modify test method
    对http对象，添加请求时间限制：10 seconds

--- module1.py
+++ module1.py
@@ -1,50 +1,59 @@
-import httplib2
-import re
+import re, math, httplib2, MySQLdb, threading
 from htmlcrawler import HtmlCrawler
-import MySQLdb
-
-# crawler test module
 
 # url links
-# these links are the diferent news blocks' url
+# these links are the different news blocks' url
 url_links = {
     'news': 'http://physics.scu.edu.cn/News/index.asp?ClassID=3',
     'notice': 'http://physics.scu.edu.cn/News/index.asp?ClassID=1',
     'recruit': 'http://physics.scu.edu.cn/News/index.asp?ClassID=16',
-    'employment': 'http://physics.scu.edu.cn/News/index.asp?ClassID=15'
+    'employment': 'http://physics.scu.edu.cn/News/index.asp?ClassID=15',
+    'science': 'http://physics.scu.edu.cn/News/index.asp?ClassID=2'
 }
 
 
-url_base = 'http://physics.scu.edu.cn'  # to get some image source
+url_base = 'http://physics.scu.edu.cn'  # to get image sources
 pattern = '<a href=\"(/News/news_show\.asp\?ArticleID=[0-9]+)\" title=\"(.*?)\">'  # pattern used to get news_list
-http = httplib2.Http('.cache')  # http object, use cache server
-
-# initialize the Html parser
-test_parser = HtmlCrawler(url_base=url_base, encoding='GB2312')
-
-# get the main page of a certain block, such this: News
-# you can select: 'Notice', 'Recruit', 'Employment'
-html_content = test_parser.handle_content(http=http, url=url_links['news'])
-
-# get the news list
-news_links, news_title = test_parser.handle_news_list(html=html_content, pattern=re.compile(pattern, re.DOTALL))
-news_title = [(ele.split('\r\n')) for ele in news_title]
-news_title = [(e[0][5:], e[1][5:]) for e in news_title]  # element type is tuple(news_tiltle, datetime)
-
-# scan the news list, and store news files
-files_path, image_links = test_parser.handle_news_files(news_links=news_links, dir='/Users/zhouming/Desktop/temp/')
-print(files_path)
-print(image_links)
-# combine news_links with file url
 
 
-# database operations test module
-conn = MySQLdb.connect(host='localhost', user='root', passwd='961727', db='crawler')
-conn.set_character_set('utf8')
-cursor = conn.cursor()
-for i in range(len(files_path)):
-    cursor.execute('insert into article(news_title, news_date, news_url, img_url) values(%s,%s,%s,%s)',
-                   [news_title[i][0], news_title[i][1], files_path[i], image_links[i]])
-conn.commit()
-cursor.close()
-conn.close()
+def crawler(key, value):
+    http = httplib2.Http('.cache', timeout=10)
+    test_parser = HtmlCrawler(url_base=url_base, encoding='GB2312', http=http, dir_base=key)
+    print('Start decode block: ', key, '...')
+    html_content = test_parser.handle_content(url=value)
+
+    # get page num
+    page_num = math.ceil(int(re.findall(string=html_content, pattern='<b>([0-9]+)</b>')[0]) / 10)
+
+    for idx in range(page_num):
+        # get the news list, 're.DOCTALL' accepts '\r\n'
+        print('---Page at: ', idx)
+        html_content = test_parser.handle_content(url=value + '&page=' + str(idx + 1))
+        if isinstance(html_content, str):
+            news_links, news_spam = test_parser.handle_news_list(html=html_content, pattern=re.compile(pattern, re.DOTALL))
+            # print('->got links')
+            files_path, title_and_img = test_parser.handle_news_files(news_links=news_links, dirs_pa=news_spam)
+        else:
+            print('->Type is wrong, type: ', type(html_content))
+    print(key, 'Done!')
+
+
+for news_name, url in url_links.items():
+    crawler(news_name, url)
+
+# class my_thread(threading.Thread):
+#     def __init__(self, key, value):
+#         threading.Thread.__init__(self)
+#         self.key = key
+#         self.value = value
+
+#     def run(self):
+#         print('开始线程：', self.key)
+#         crawler(self.key, self.value)
+#         print('结束线程：', self.key)
+
+
+# thread_list = []
+# [threads.append(my_thread(key, value)) for key, value in url_links.items()]
+# [ele.start() for ele in thread_list]
+# [ele.join() for ele in thread_list]
